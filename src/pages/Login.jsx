b6017e9073f90e00c79fee3da1b6b0922a9fec0e
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2023-01-25 16:41:33 -0300

    Implementados os inputs de email e senha juntamente com o botão, Req 2-3

---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <label htmlFor="email">
+        Email:
+        <input data-testid="email-input" type="email" id="email" />
+      </label>
+      <label htmlFor="password">
+        Password:
+        <input data-testid="password-input" type="password" id="password" />
+      </label>
+      <button data-testid="login-submit-btn">Enter</button>
+    </div>
+  );
+}
