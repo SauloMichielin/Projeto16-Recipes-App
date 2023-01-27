3a274e1fb71e13be73265a20f78c889216a9a9e8
GabrielVanzo <gabriel.vanzo98@gmail.com>
2023-01-27 19:13:50 -0300

    Elaboração da Tela de Profile + Testes, Requisitos 57 à 62

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 
 function Profile() {
+  const history = useHistory();
+  const getEmailLS = localStorage.getItem('user');
+  const email = getEmailLS.split('"')[3];
   return (
     <section>
       <Header title="Profile" />
+      <span>Email:</span>
+      {' '}
+      <span data-testid="profile-email">{ email }</span>
+      <br />
+      <button
+        type="button"
+        onClick={ () => history.push('/done-recipes') }
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        onClick={ () => history.push('/favorite-recipes') }
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        onClick={ () => {
+          localStorage.clear();
+          history.push('/');
+        } }
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
       <Footer />
     </section>
   );
