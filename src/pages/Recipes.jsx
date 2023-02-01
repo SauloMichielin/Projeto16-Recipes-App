42cd36c08e1be6250ca487ad0ff8ea45c20ebcc3
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2023-02-01 15:46:56 -0300

    Implementada a função de aparecer as 12 primeiras receitas ao iniciar a pagina, req 19

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import Drinks from './Drinks';
+import Meals from './Meals';
 
-export default function Recipe() {
+export default function Recipes() {
+  const history = useHistory();
   return (
-    <div>Receitas</div>
+    <div>
+      {
+        history.location.pathname === '/meals' ? <Meals /> : <Drinks />
+      }
+    </div>
   );
 }
