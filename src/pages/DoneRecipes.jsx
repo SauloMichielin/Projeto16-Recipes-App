47763fd27e9bb87f502dd67cf26cd73cb465a39b
Arthur Murilo Oldenburg <oldenburgarthurmurilo@gmail.com>
2023-02-02 20:13:52 -0300

    Tela de done recipe

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { require } from 'clipboard-copy';
 import Header from '../Components/Header';
 import shareImg from '../images/shareIcon.svg';
 import localSto from '../mockTemp';
@@ -8,6 +9,8 @@ function DoneRecipes() {
   const [localS, setLocalS] = useState([]);
   const [filter, setFilter] = useState('All');
   const history = useHistory();
+  const [showOrHide, setShowOrHide] = useState(0);
+  const copy = require('clipboard-copy');
   const items = [];
   useEffect(() => {
     const lS = JSON.parse(localStorage.getItem('doneRecipe'));
@@ -41,65 +44,95 @@ function DoneRecipes() {
   return (
     <section>
       <Header title="Done Recipes" />
-      <button
-        data-testid="filter-by-meal-btn"
-        onClick={ () => setFilter('Meals') }
-      >
+      <button data-testid="filter-by-meal-btn" onClick={ () => setFilter('Meals') }>
         Meals
       </button>
-      <button
-        data-testid="filter-by-drink-btn"
-        onClick={ () => setFilter('Drinks') }
-      >
+      <button data-testid="filter-by-drink-btn" onClick={ () => setFilter('Drinks') }>
         Drinks
       </button>
-      <button
-        data-testid="filter-by-all-btn"
-        onClick={ () => setFilter('All') }
-      >
+      <button data-testid="filter-by-all-btn" onClick={ () => setFilter('All') }>
         All
       </button>
-      { items.map((e, i) => (
-        <div key={ i }>
-          <div onClick={ () => history.push(`/meals/${e.id}`) } aria-hidden="true">
-            <img
-              data-testid={ `${i}-horizontal-image` }
-              src={ e.image }
-              alt="Foto da receita"
-            />
-            <p
-              data-testid={ `${i}-horizontal-name` }
-            >
-              {e.name}
-            </p>
-          </div>
-          <p
-            data-testid={ `${i}-horizontal-top-text` }
-          >
-            {`${e.nationality} - ${e.category}`}
-          </p>
-          <p
-            data-testid={ `${i}-horizontal-done-date` }
-          >
-            { e.doneDate }
-          </p>
-          <img
-            src={ shareImg }
-            alt="Compartilhar"
-            data-testid={ `${i}-horizontal-share-btn` }
-            onClick={ () => {
-              navigator.clipboard.writeText(`/${e.type}/${e.id}`);
-              global.alert('Link copied!');
-            } }
-            aria-hidden="true"
-          />
-          <p
-            data-testid={ `${i}-${0}-horizontal-tag` }
-          >
-            { e.tags.length === 0 ? '' : `${e.tags[0]}, ${e.tags[1]}` }
-          </p>
-        </div>
-      ))}
+      {
+        items.map((e, i) => (
+          (e.type === 'meals' ? (
+            <div key={ i }>
+              <div
+                onClick={ () => history.push(`/${e.type}/${e.id}`) }
+                aria-hidden="true"
+              >
+                <img
+                  data-testid={ `${i}-horizontal-image` }
+                  src={ e.image }
+                  alt="Foto da receita"
+                />
+                <h2 data-testid={ `${i}-horizontal-name` }>{e.name}</h2>
+              </div>
+              <h3 data-testid={ `${i}-horizontal-top-text` }>
+                {`${e.nationality} - ${e.category}`}
+              </h3>
+              <h4 data-testid={ `${i}-horizontal-done-date` }>{ e.doneDate }</h4>
+              <h5 data-testid={ `${i}-${e.tags[i]}-horizontal-tag` }>
+                { e.tags.length > 0 && `${e.tags[0]}, ${e.tags[1]}` }
+              </h5>
+              <div
+                data-testid={ `${i}-horizontal-share-btn` }
+                onClick={ () => {
+                  copy(`http://localhost:3000/${e.type}/${e.id}`);
+                  setShowOrHide(+e.id);
+                } }
+                aria-hidden="true"
+              >
+                <img
+                  src={ shareImg }
+                  alt="Compartilhar"
+                />
+                <br />
+                <h4
+                  style={ { display: +showOrHide === +e.id ? 'content' : 'none' } }
+                >
+                  Link copied!
+                </h4>
+              </div>
+            </div>
+          ) : (
+            <div key={ i }>
+              <div
+                onClick={ () => history.push(`/${e.type}/${e.id}`) }
+                aria-hidden="true"
+              >
+                <img
+                  data-testid={ `${i}-horizontal-image` }
+                  src={ e.image }
+                  alt="Foto da receita"
+                />
+                <h2 data-testid={ `${i}-horizontal-name` }>{e.name}</h2>
+              </div>
+              <h3 data-testid={ `${i}-horizontal-top-text` }>{e.alcoholicOrNot}</h3>
+              <h4 data-testid={ `${i}-horizontal-done-date` }>{ e.doneDate }</h4>
+              <div
+                data-testid={ `${i}-horizontal-share-btn` }
+                onClick={ () => {
+                  copy(`http://localhost:3000/${e.type}/${e.id}`);
+                  setShowOrHide(+e.id);
+                  console.log('clickou');
+                } }
+                aria-hidden="true"
+              >
+                <img
+                  src={ shareImg }
+                  alt="Compartilhar"
+                />
+                <br />
+                <h4
+                  style={ { display: +showOrHide === +e.id ? 'content' : 'none' } }
+                >
+                  Link copied!
+                </h4>
+              </div>
+            </div>
+          ))))
+      }
     </section>
   );
 }
